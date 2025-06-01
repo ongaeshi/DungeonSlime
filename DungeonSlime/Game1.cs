5182af8e187c398a99d6e9d8b12ab68da0346d04
ongaeshi <ongaeshi0621@gmail.com>
2025-06-02 00:18:46 +0900

    Revert "feat: Chapter 7 開始（テスト）"
    This reverts commit 9d4cc5d1736285dbadf8fc6d087c0c556c63725f.

--- DungeonSlime/Game1.cs
+++ DungeonSlime/Game1.cs
@@ -13,14 +13,7 @@ namespace DungeonSlime
         // The MonoGame logo texture
         private Texture2D _logo;
 
-        private Texture2D _textureAtlas;
-        private Rectangle _paddleSourceRect;
-        private Rectangle _ballSourceRect;
-        private Vector2 _leftPaddlePosition;
-        private Vector2 _rightPaddlePosition;
-        private Vector2 _ballPosition;
-
-        public Game1()
+        public Game1() 
             : base("Dungeon Slime", 1280, 720, fullScreen: false)
         {
         }
@@ -34,9 +27,7 @@ namespace DungeonSlime
 
         protected override void LoadContent()
         {
-            _textureAtlas = Content.Load<Texture2D>("pong-atlas");
-            _paddleSourceRect = new Rectangle(0, 0, 32, 32);
-            _ballSourceRect = new Rectangle(32, 0, 32, 32);
+            _logo = Content.Load<Texture2D>("images/logo");
         }
 
         protected override void Update(GameTime gameTime)
@@ -53,18 +44,54 @@ namespace DungeonSlime
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            _leftPaddlePosition = new Vector2(50, GraphicsDevice.Viewport.Height / 2 - _paddleSourceRect.Height / 2);
-            _rightPaddlePosition = new Vector2(GraphicsDevice.Viewport.Width - 50 - _paddleSourceRect.Width, GraphicsDevice.Viewport.Height / 2 - _paddleSourceRect.Height / 2);
-            _ballPosition = new Vector2(GraphicsDevice.Viewport.Width / 2 - _ballSourceRect.Width / 2, GraphicsDevice.Viewport.Height / 2 - _ballSourceRect.Height / 2);
+            // The bounds of the icon within the texture.
+            Rectangle iconSourceRect = new Rectangle(0, 0, 128, 128);
 
-            SpriteBatch.Begin();
+            // The bounds of the word mark within the texture.
+            Rectangle wordmarkSourceRect = new Rectangle(150, 34, 458, 58);
 
-            // All draw calls use the same texture, so there is no texture swapping!
-            SpriteBatch.Draw(_textureAtlas, _leftPaddlePosition, _paddleSourceRect, Color.White);
-            SpriteBatch.Draw(_textureAtlas, _rightPaddlePosition, _paddleSourceRect, Color.White);
-            SpriteBatch.Draw(_textureAtlas, _ballPosition, _ballSourceRect, Color.White);
+            // Begin the sprite batch to prepare for rendering.
+            SpriteBatch.Begin(sortMode: SpriteSortMode.FrontToBack);
 
+            // Draw only the icon portion of the texture.
+            SpriteBatch.Draw(
+                _logo,              // texture
+                new Vector2(        // position
+                    Window.ClientBounds.Width,
+                    Window.ClientBounds.Height) * 0.5f,
+                iconSourceRect,     // sourceRectangle
+                Color.White,        // color
+                0.0f,               // rotation
+                new Vector2(        // origin
+                    iconSourceRect.Width,
+                    iconSourceRect.Height) * 0.5f,
+                1.0f,               // scale
+                SpriteEffects.None, // effects
+                1.0f                // layerDepth
+            );
+
+            // Draw only the word mark portion of the texture.
+            SpriteBatch.Draw(
+                _logo,              // texture
+                new Vector2(        // position
+                  Window.ClientBounds.Width,
+                  Window.ClientBounds.Height) * 0.5f,
+                wordmarkSourceRect, // sourceRectangle
+                Color.White,        // color
+                0.0f,               // rotation
+                new Vector2(        // origin
+                  wordmarkSourceRect.Width,
+                  wordmarkSourceRect.Height) * 0.5f,
+                1.0f,               // scale
+                SpriteEffects.None, // effects
+                0.0f                // layerDepth
+            );
+
+            // Always end the sprite batch when finished.
             SpriteBatch.End();
+
+
+            base.Draw(gameTime);
         }
     }
 }
