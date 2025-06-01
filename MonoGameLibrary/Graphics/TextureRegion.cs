6a84f76aa6fb191d10be28964959d8860187ec1c
ongaeshi <ongaeshi0621@gmail.com>
2025-06-02 00:20:11 +0900

    feat: Add TextureRegion class

---
+++ MonoGameLibrary/Graphics/TextureRegion.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MonoGameLibrary.Graphics;
+
+/// <summary>
+/// Represents a rectangular region within a texture.
+/// </summary>
+public class TextureRegion
+{
+
+}
