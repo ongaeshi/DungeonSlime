db63b9f92e5fc90358bb7b249960159fe809f2d5
ongaeshi <ongaeshi0621@gmail.com>
2025-06-01 23:50:56 +0900

    feat: sourceRectangle 2つのスプライトに分けて描画

--- DungeonSlime/Game1.cs
+++ DungeonSlime/Game1.cs
@@ -44,24 +44,48 @@ namespace DungeonSlime
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
+            // The bounds of the icon within the texture.
+            Rectangle iconSourceRect = new Rectangle(0, 0, 128, 128);
+
+            // The bounds of the word mark within the texture.
+            Rectangle wordmarkSourceRect = new Rectangle(150, 34, 458, 58);
+
             // Begin the sprite batch to prepare for rendering.
             SpriteBatch.Begin();
 
-            // Draw the logo texture
+            // Draw only the icon portion of the texture.
             SpriteBatch.Draw(
-                texture: _logo,
-                position: new Vector2(
+                _logo,              // texture
+                new Vector2(        // position
                     Window.ClientBounds.Width,
                     Window.ClientBounds.Height) * 0.5f,
-                sourceRectangle: null, 
-                color: Color.White * 0.5f,
-                rotation: 0,
-                origin: new Vector2(
-                    _logo.Width,
-                    _logo.Height) * 0.5f,
-                scale: 1.0f,
-                effects: SpriteEffects.None, // 縦や横軸のフリップ
-                layerDepth: 0.0f); // テクスチャがレンダリングされる深さ
+                iconSourceRect,     // sourceRectangle
+                Color.White,        // color
+                0.0f,               // rotation
+                new Vector2(        // origin
+                    iconSourceRect.Width,
+                    iconSourceRect.Height) * 0.5f,
+                1.0f,               // scale
+                SpriteEffects.None, // effects
+                0.0f                // layerDepth
+            );
+
+            // Draw only the word mark portion of the texture.
+            SpriteBatch.Draw(
+                _logo,              // texture
+                new Vector2(        // position
+                  Window.ClientBounds.Width,
+                  Window.ClientBounds.Height) * 0.5f,
+                wordmarkSourceRect, // sourceRectangle
+                Color.White,        // color
+                0.0f,               // rotation
+                new Vector2(        // origin
+                  wordmarkSourceRect.Width,
+                  wordmarkSourceRect.Height) * 0.5f,
+                1.0f,               // scale
+                SpriteEffects.None, // effects
+                0.0f                // layerDepth
+            );
 
             // Always end the sprite batch when finished.
             SpriteBatch.End();
