78b7383c2938affd6ed39277e2da986855e95736
ongaeshi <ongaeshi0621@gmail.com>
2025-06-09 02:30:48 +0900

    feat: Add Sprite class and TextureAtlas.CreateSprite() method

--- MonoGameLibrary/Graphics/TextureAtlas.cs
+++ MonoGameLibrary/Graphics/TextureAtlas.cs
@@ -34,6 +34,17 @@ public class TextureAtlas
     public Texture2D Texture { get; set; }
 
     /// <summary>
+    /// Creates a new sprite using the region from this texture atlas with the specified name.
+    /// </summary>
+    /// <param name="regionName">The name of the region to create the sprite with.</param>
+    /// <returns>A new Sprite using the texture region with the specified name.</returns>
+    public Sprite CreateSprite(string regionName)
+    {
+        TextureRegion region = GetRegion(regionName);
+        return new Sprite(region);
+    }
+
+    /// <summary>
     /// Creates a new region and adds it to this texture atlas.
     /// </summary>
     /// <param name="name">The name to give the texture region.</param>
